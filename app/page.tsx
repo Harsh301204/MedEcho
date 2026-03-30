33b63d84ea09f67d2e9d15a7afa1c2d13aafdead
Harsh Sharma <harshsharma301a@gmail.com>
2026-03-30 14:19:34 +0530

    tested Shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -8,25 +8,14 @@ import {
   CardFooter,
   CardTitle
 } from "@/components/ui/card";
+import HeroSectionOne from "@/components/hero-section-demo-1";
+import BentoGridThirdDemo from "@/components/bento-grid-demo-3";
 
 export default function Home() {
   return (
     <div>
-      Starting MedEcho
-      <Button>Click Me</Button>
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-          {/* <CardAction>Card Action</CardAction> */}
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
+      <HeroSectionOne/>
+      <BentoGridThirdDemo/>
     </div>
   );
 }
