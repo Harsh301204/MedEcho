ad66317c242491402252994cfc9be0be1db68e89
Harsh Sharma <harshsharma301a@gmail.com>
2026-03-30 12:26:02 +0530

    initialized shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardFooter,
+  CardTitle
+} from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div>
       Starting MedEcho
+      <Button>Click Me</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          {/* <CardAction>Card Action</CardAction> */}
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
