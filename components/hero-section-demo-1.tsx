b615ccf9ed9d5d0753b4ed703c2c4f100183494b
Harsh Sharma <harshsharma301a@gmail.com>
2026-03-31 16:22:23 +0530

    Added Auth Components

--- components/hero-section-demo-1.tsx
+++ components/hero-section-demo-1.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 
+import { UserButton, useUser } from "@clerk/nextjs";
 import { motion } from "motion/react";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function HeroSectionOne() {
   return (
@@ -104,15 +107,24 @@ export default function HeroSectionOne() {
 }
 
 const Navbar = () => {
+  const user = useUser();
   return (
     <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
       <div className="flex items-center gap-2">
         <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
         <h1 className="text-base font-bold md:text-2xl">Aceternity UI</h1>
       </div>
+      {!user ? 
+        <Link href={'/sign-in'}>
       <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
         Login
-      </button>
+      </button> 
+        </Link> :
+        <div className="flex items-center gap-5">
+          <UserButton/>
+          <Button>Dashboard</Button>
+        </div>
+      }
     </nav>
   );
 };
