b837ce3c8340313400f94a450245d53ac9dd6847
Harsh Sharma <harshsharma301a@gmail.com>
2026-07-24 23:08:45 +0530

    added pro badge on doctors

--- app/(routes)/dashboard/_components/DoctorAgentCard.tsx
+++ app/(routes)/dashboard/_components/DoctorAgentCard.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 import DialogSession from "./DialogSession";
+import { Badge } from "@/components/ui/badge";
 
 export type Doctor = {
   id: number;
@@ -18,10 +19,14 @@ type DoctorProps = {
 }
 
 function DoctorAgentCard({props} : DoctorProps) {
-  return <div className="flex flex-col justify-center items-center rounded-xl p-2 ">
+  // return <div className="flex flex-col justify-center items-center rounded-xl p-2 ">
+  return <div className="relative">
+  {props.subscriptionRequired && <Badge className="absolute p-3 right-0 text-md">
+      Premium
+    </Badge>} 
     <Image className="w-full h-[250] object-cover rounded-2xl" src={props.image} alt="img" width={200} height={300}/>
-    <p className="font-bold text-xl">{props.specialist}</p>
-    <p className="line-clamp-2 text-sm text-gray-500">{props.description}</p>
+    <p className="font-bold text-xl text-center">{props.specialist}</p>
+    <p className="line-clamp-2 text-sm text-gray-500 text-center">{props.description}</p>
     <DialogSession/>
   </div>;
 }
--- app/(routes)/dashboard/billing/page.tsx
+++ app/(routes)/dashboard/billing/page.tsx
@@ -2,8 +2,8 @@ import { PricingTable } from '@clerk/nextjs'
 
 export default function billing() {
   return (
-    <div>
-        <h1 className='font-bold text-2xl'>Subscription Plans</h1>
+    <div className='px-10 md:px-24 lg:px-48'>
+        <h1 className='font-bold text-3xl mb-7'>Subscription Plans</h1>
       <PricingTable />
     </div>
   )
