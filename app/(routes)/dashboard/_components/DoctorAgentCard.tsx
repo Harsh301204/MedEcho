08e7823b343877d23c0718af6ba0476e054f1944
Harsh Sharma <harshsharma301a@gmail.com>
2026-07-27 23:55:39 +0530

    updated UI for doctor agent card

--- app/(routes)/dashboard/_components/DoctorAgentCard.tsx
+++ app/(routes)/dashboard/_components/DoctorAgentCard.tsx
@@ -30,11 +30,11 @@ export default async function DoctorAgentCard({props} : DoctorProps) {
 
 
   // return <div className="flex flex-col justify-center items-center rounded-xl p-2 ">
-  return <div className="relative">
+  return <div className={`relative`}>
   {props.subscriptionRequired && <Badge className="absolute p-3 right-0 text-md">
       Premium
     </Badge>} 
-    <Image className="w-full h-[250] object-cover rounded-2xl" src={props.image} alt="img" width={200} height={300}/>
+    <Image className={` w-full h-[250] object-cover rounded-2xl`} src={props.image} alt="img" width={200} height={300}/>
     <p className="font-bold text-xl text-center">{props.specialist}</p>
     <p className="line-clamp-2 text-sm text-gray-500 text-center">{props.description}</p>
     <DialogSession props={shouldDisable}/>
