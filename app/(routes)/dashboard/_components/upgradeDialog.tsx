b98aca129eabd98163d808154c1d881918cc7fd4
Harsh Sharma <harshsharma301a@gmail.com>
2026-07-28 16:03:41 +0530

    uprade dialog routing

--- app/(routes)/dashboard/_components/upgradeDialog.tsx
+++ app/(routes)/dashboard/_components/upgradeDialog.tsx
@@ -2,6 +2,7 @@
 
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogHeader,
@@ -10,46 +11,34 @@ import {
 } from "@/components/ui/dialog";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-
-
-type UpgradeDialogProps = {
-  open: boolean;
-  onOpenChange: (open: boolean) => void;
-};
+import Link from "next/link";
 
 export default function UpgradeDialog() {
   return (
     <Dialog>
-              <DialogTrigger className={cn(buttonVariants(), "mt-2 w-full")}>
-            Upgrade to Pro 
+      <DialogTrigger className={cn(buttonVariants(), "mt-2 w-full")}>
+        Start Consultation
       </DialogTrigger>
       <DialogContent className="sm:max-w-md">
-
         <DialogHeader>
-          <DialogTitle>
-            Upgrade to MedEcho Pro
-          </DialogTitle>
+          <DialogTitle>Upgrade to MedEcho Pro</DialogTitle>
 
           <DialogDescription>
-            This specialist is available only for Pro members.
-            Upgrade to unlock premium consultations.
+            This specialist is available only for Pro members. Upgrade to unlock
+            premium consultations.
           </DialogDescription>
         </DialogHeader>
 
         <div className="space-y-3 pt-4">
-          <Button className="w-full">
-            Upgrade Now
-          </Button>
+            <Link href="/dashboard/billing">
+            <Button className="w-full">Upgrade Now</Button>
+            </Link>
+          
 
-          <Button
-            variant="outline"
-            className="w-full"
-            // onClick={() => onOpenChange(false)}
-          >
+        <DialogClose className={cn(buttonVariants({ variant: "outline" }), "w-full")}>
             Maybe Later
-          </Button>
+          </DialogClose>
         </div>
-
       </DialogContent>
     </Dialog>
   );
