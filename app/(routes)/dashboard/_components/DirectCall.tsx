7cf148f104c7cb17ada61cbc9ff145cac7eeea28
Harsh Sharma <harshsharma301a@gmail.com>
2026-07-29 13:01:54 +0530

    added loader while call connects

--- app/(routes)/dashboard/_components/DirectCall.tsx
+++ app/(routes)/dashboard/_components/DirectCall.tsx
@@ -5,6 +5,8 @@ import axios from 'axios'
 import { Doctor } from './DoctorAgentCard'
 import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+import { Loader2 } from 'lucide-react'
 
 
 type DirectCallProps = {
@@ -14,8 +16,11 @@ type DirectCallProps = {
 
 export default function DirectCall({selectedDoctor} : DirectCallProps) {
 
+    const [loading , setLoading] = useState<boolean>(false)
+
     const router = useRouter()
     const onStartConsultation = async () => {
+    setLoading(true)
     const result = await axios.post('/api/session-chat' , {
       notes : `The user wants to directly consult a ${selectedDoctor.specialist}`,
       selectedDoctor : selectedDoctor
@@ -25,9 +30,19 @@ export default function DirectCall({selectedDoctor} : DirectCallProps) {
       router.push('/dashboard/medical-agent/' + result.data[0]?.sessionId)
 
     }
+    setLoading(false)
   }
   return (
-    <Button className={`w-full mt-2`} onClick={() => onStartConsultation()}>Start Consultation</Button>
+    <>
+        {loading && (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
+        <Loader2 className="h-10 w-10 animate-spin text-white" />
+      </div>
+        )}
+
+        <Button className={`w-full mt-2`} onClick={() => onStartConsultation()}>Start Consultation</Button>
+    </>
+    
   )
 }
 
